8ddbb755573d6511547400df0f1e9519cc734440
Camilo Pineda <kmylop@hotmail.com>
2020-09-23 00:49:41 -0400

    styled home, cards, skills and projects

--- portfolio/src/Projects.jsx
+++ portfolio/src/Projects.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { CardDeck } from "react-bootstrap";
 import MyCard from "./components/MyCard";
+import "./Projects.css"
 const dataProjects = {
   projects: [
+    {
+      srcImgProject: "./images/Projects/BeerLovers.png",
+      title: "BeerLovers",
+      projectDescription: "React,Node.js,Postgresql",
+    },
+    {
+      srcImgProject: "./images/Projects/Scheduler.png",
+      title: "Scheduler",
+      projectDescription: "React,Node.js,Postgresql",
+    },
+    {
+      srcImgProject: "./images/Projects/Jungle.png",
+      title: "Jungle",
+      projectDescription: "React,Node.js,Postgresql",
+    },
     {
       srcImgProject: "./images/Projects/BeerLovers.png",
       title: "BeerLovers",
@@ -36,11 +52,11 @@ function Projects() {
   }) : undefined;
   return (
     <article className="mainArticle">
-      <CardDeck
-        style={{ marginLeft: "auto", marginRight: "auto", marginTop: "3vh",width: "80%" }}
+      <div className="cardsContainer"
+        // style={{ marginLeft: "auto", marginRight: "auto", marginTop: "3vh",width: "80%" }}
       >
         {cards}
-      </CardDeck>
+      </div>
     </article>
   );
 }
--- portfolio/src/components/MyCard.jsx
+++ portfolio/src/components/MyCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap';
+import "./MyCard.css"
 
 function MyCard(props) {
   const { srcImgProject, title, projectDescription} = props;
   return (
     <>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "40vh", marginLeft:"8vh",marginBottom:"3vh"}}>
         <Card.Img
           variant="top"
           src={srcImgProject}
-          style={{ height: "18rem" }}
+          className="cardImage"
+          // style={{ height: "18rem" }}
         />
-        <Card.Body style={{ color: "black", backgroundColor:"blue" }}>
+        <Card.Body style={{ color: "black", backgroundColor: "blue" }}>
           <Card.Title>{title}</Card.Title>
-          <Card.Text >
-            {projectDescription}
-          </Card.Text>
+          <Card.Text>{projectDescription}</Card.Text>
           <Button variant="primary">Go {title}</Button>
         </Card.Body>
       </Card>
