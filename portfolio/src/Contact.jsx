5f2207cf5a221a83d15a1672da8279dfbd716974
Camilo Pineda <kmylop@hotmail.com>
2020-10-08 12:26:08 -0400

    responsive contact page fixed

--- portfolio/src/Contact.jsx
+++ portfolio/src/Contact.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Contact.css"
+import "./Contact.css";
 
 function Contact() {
   return (
-    <>
+    <div className="mainArticle">
       <h1 className="title">CONTACT</h1>
       <article className="contactCard">
         <header className="contactCardHeader">
@@ -12,23 +12,39 @@ function Contact() {
           <hr className="hrContact"></hr>
           <div className="contactCardBody">
             <div className="contactWay">
-              <img
-                className="iconContact"
-                src="./images/contact/envelope-solid.svg"
-              />
+              <div className="iconContact">
+                <img
+                  className="iconContact"
+                  src="./images/contact/mobile-alt-solid.svg"
+                  alt="contactImg"
+                />
+              </div>
+              <div className="contactTxt">438-630-1247</div>
+            </div>
+            <div className="contactWay">
+              <div className="iconContact">
+                <img
+                  className="iconContact"
+                  src="./images/contact/envelope-solid.svg"
+                  alt="contactImg"
+                />
+              </div>
               <div className="contactTxt">camilopinedadev@gmail.com</div>
             </div>
             <div className="contactWay">
-              <img
-                className="iconContact"
-                src="./images/contact/envelope-solid.svg"
-              />
-              <div className="contactTxt">camilopinedadev@gmail.com</div>
+              <div className="iconContact">
+                <img
+                  className="iconContact"
+                  src="./images/contact/globe-americas-solid.svg"
+                  alt="contactImg"
+                />
+              </div>
+              <div className="contactTxt">www.camilopinedadev.com</div>
             </div>
           </div>
         </header>
       </article>
-    </>
+    </div>
   );
 }
 
