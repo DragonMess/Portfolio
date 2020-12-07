e4a15a3e0421896a0f0d825d2d48c75ee182e2ad
Camilo Pineda <kmylop@hotmail.com>
2020-12-07 13:54:55 -0500

    chaged name homepage

---
+++ build/precache-manifest.6501e9affa4a66854c76573302a5394a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c71a299a4c0ba354c89b732ecc01d4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b42f2b307e8b9df8ec97",
+    "url": "/Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4ec52022f225e3b19147",
+    "url": "/Portfolio/static/css/main.b4f666de.chunk.css"
+  },
+  {
+    "revision": "b42f2b307e8b9df8ec97",
+    "url": "/Portfolio/static/js/2.afc62bc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.afc62bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec52022f225e3b19147",
+    "url": "/Portfolio/static/js/main.92efd79f.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
